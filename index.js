b74ce9e6d42187c85e042d7d583b5c41e5a4b962
taqi <taqiedits@gmail.com>
2022-03-29 22:11:38 +0200

    done 29 marca

---
+++ index.js
@@ -0,0 +1,9 @@
+function teoria() {
+  window.location.href = 'html-podstrony/teoria.html'
+}
+function praktyka() {
+  window.location.href = 'html-podstrony/praktyka.html'
+}
+function powrotIndex() {
+  window.location.href = '../index.html'
+}
