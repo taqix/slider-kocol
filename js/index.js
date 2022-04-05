02c0146287267f140409e1b5e45b83cd307d5d6d
porucznik-huncwot <kamil4.zag@gmail.com>
2022-04-05 21:06:45 +0200

    mega-update-1
    05.04.2022

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function teoria() {
-  window.location.href = 'html-podstrony/teoria.html'
-}
-function praktyka() {
-  window.location.href = 'html-podstrony/praktyka.html'
-}
-function powrotIndex() {
-  window.location.href = '../index.html'
-}
---
+++ js/link-functions.js
@@ -0,0 +1,23 @@
+function teoria() {
+  window.location.href = 'html-podstrony/teoria.html';
+}
+
+function praktyka() {
+  window.location.href = 'html-podstrony/praktyka.html';
+}
+
+function backToPraktyka() {
+  window.location.href = 'praktyka.html';
+}
+
+function powrotIndex() {
+  window.location.href = '../index.html';
+}
+
+function cwiczenie1(){
+  window.location.href = 'praktyka-ex1.html'
+}
+
+function cwiczenie2(){
+  window.location.href = 'praktyka-ex2.html'
+}
