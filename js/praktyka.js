7e1d9325c78bc7a5b56f9c6ec7908c96105aea02
taqi <taqiedits@gmail.com>
2022-04-19 22:09:23 +0200

    end essa udalo sie mamy to kocury

--- js/praktyka.js
+++ js/praktyka.js
@@ -4,20 +4,24 @@ showSlides(slideIndex)
 function plusSlides(n) {
   showSlides((slideIndex += n))
 }
-
+function currentSlider(n) {
+  showSlides((slideIndex = n))
+}
 function showSlides(n) {
-  let i
+  const dots = document.querySelectorAll('.dot')
   const slides = document.querySelectorAll('.slidy')
-
   if (n > slides.length) {
     slideIndex = 1
   }
   if (n < 1) {
     slideIndex = slides.length
   }
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none'
   }
-
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('active')
+  }
+  dots[slideIndex - 1].classList.add('active')
   slides[slideIndex - 1].style.display = 'block'
 }
